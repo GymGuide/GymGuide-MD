a877470948f7d08aff64a800375d73c927dbc27c
fnilvuwu <mamatmks45@gmail.com>
2023-12-08 22:31:11 +0800

    minor fix layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,10 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
     implementation("org.tensorflow:tensorflow-lite-support:0.4.2")
 
-    // Retrofit
+    // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
